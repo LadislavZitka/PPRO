0b0645ca90a89ee3749662bab7998394a20cc277
Ladislav Zitka <zitkala1@uhk.cz>
2025-01-09 20:03:17 +0100

    exercise 9 and 10

---
+++ ppro/src/main/java/cz/uhk/kppro/log/LoggingAspect.java
@@ -0,0 +1,17 @@
+package cz.uhk.kppro.log;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    @Before("execution(* cz.uhk.kppro.*.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        System.out.println("Calling method: " + className + "." + methodName);
+    }
+}
