5a61729d0c3ff0cf8d8d8adf204d0d09f6da86f4
Ladislav Zitka <zitkala1@uhk.cz>
2025-01-09 13:56:04 +0100

    exercise 8

---
+++ ppro/src/main/java/cz/uhk/kppro/rest/DriverRestController.java
@@ -0,0 +1,39 @@
+package cz.uhk.kppro.rest;
+
+import cz.uhk.kppro.model.Driver;
+import cz.uhk.kppro.service.DriverService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class DriverRestController {
+    private DriverService driverService;
+
+    @Autowired
+    public DriverRestController(DriverService driverService) {
+        this.driverService = driverService;
+    }
+
+    @GetMapping("/driver/{id}")
+    public Driver driverDetail(@PathVariable Long id){
+        return driverService.getDriver(id);
+    }
+
+    @PostMapping("/driver")
+    public boolean driverCreate(@Valid @RequestBody Driver driver){
+        driverService.addDriver(driver);
+        return true;
+    }
+
+    @PutMapping("/driver")
+    public boolean driverUpdate(@Valid @RequestBody Driver driver){
+        driverService.updateDriver(driver);
+        return true;
+    }
+
+    @DeleteMapping("/driver/{id}")
+    public Driver driverDelete(@PathVariable Long id){
+        return driverService.deleteDriver(id);
+    }
+}
